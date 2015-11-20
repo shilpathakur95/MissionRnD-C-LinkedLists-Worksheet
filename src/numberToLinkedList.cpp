2cfd3418e7f5b7f06e5caad8725da587595b8a00
shilpathakur95 <thakur.shilpa95@gmail.com>
2015-11-20 17:59:52 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -19,6 +19,35 @@ struct node {
 	struct node *next;
 };
 
-struct node * numberToLinkedList(int N) {
-	return NULL;
+struct node * numberToLinkedList(int N) 
+{
+	struct node *head;
+	struct node *temp;
+	head = (struct node *)malloc(sizeof(struct node));
+	temp = (struct node *)malloc(sizeof(struct node));
+	head = NULL;
+	int A[10];
+	int iterator=0,iterator2;
+	if (N < 0)
+		N = N*-1;
+	else if (N == 0)
+	{
+		temp->num = 0;
+		temp->next = NULL;
+		return temp;
+	}
+	while (N > 0)
+	{
+		A[iterator] = N % 10;
+		N = N / 10;
+		iterator++;
+	}
+	for (iterator2 = 0; iterator2<iterator; iterator2++)
+	{
+		temp = (struct node *)malloc(sizeof(struct node));
+		temp->num = A[iterator2];
+		temp->next = head;
+		head = temp;
+	}
+	return  temp;
 }
